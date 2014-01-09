58054a70071ef70a7de1c580c611bfb00f56b070
Shekhar <pythonic@gmail.com>
2014-01-09 16:53:04 +0530

    users stats

---
+++ appbase/users/stats.py
@@ -0,0 +1,18 @@
+from sqlalchemy.sql import select, func
+
+import appbase.sa as sa
+from appbase.users.schema import users
+
+
+def count():
+    conn = sa.connect()
+    q = select([func.count(users.c.id)])
+    return conn.execute(q).fetchone()[0]
+
+
+def groupby_created(precision='month'):
+    conn = sa.connect()
+    month = func.date_trunc('month', users.c.created).label('month')
+    q = select([month, func.count(users.c.id)]).group_by('month').order_by('month')
+    #return conn.execute(q).fetchall()
+    return [(dt.strftime('%b %Y'), num) for (dt, num) in conn.execute(q).fetchall()]
