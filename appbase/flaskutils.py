b53c5a8da8a297611d5b7a170fc66e738665c74e
Shekhar <pythonic@gmail.com>
2014-04-06 12:11:25 +0530

    Access-Control-Allow-Origin headers

--- appbase/flaskutils.py
+++ appbase/flaskutils.py
@@ -38,7 +38,9 @@ def crossdomain(origin=None, methods=None, headers=None,
             h['Access-Control-Allow-Origin'] = origin
             h['Access-Control-Allow-Methods'] = get_methods()
             h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
+            if headers is None:
+                h['Access-Control-Allow-Headers'] = request.headers.get('Access-Control-Request-Headers', '')
+            else:
                 h['Access-Control-Allow-Headers'] = headers
             return resp
 
