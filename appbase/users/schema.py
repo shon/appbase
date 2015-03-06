968773cf75d7143432350f240c06a8033d5a071a
Shekhar <pythonic@gmail.com>
2015-03-06 19:13:10 +0530

    removing some non generic structures

--- appbase/users/schema.py
+++ appbase/users/schema.py
@@ -13,30 +13,12 @@ users = Table('users', metadata,
               Column('groups', ARRAY(String), default=[]),
               )
 
-plans = Table('plans', metadata,
-              Column_id(),
-              Column_created(),
-              Column_active(),
-              Column('name', String, nullable=False),
-              Column('description', String),
-              )
-
 groups = Table('groups', metadata,
               Column_created(),
               Column_active(),
               Column_pk_name(),
               Column('description', String),
               )
-
-subscriptions = Table('subscriptions', metadata,
-                      Column_id(),
-                      Column('user_id', None, ForeignKey('users.id', ondelete='CASCADE')),
-                      Column('plan_id', None, ForeignKey('plans.id', ondelete='CASCADE')),
-                      Column_created(),
-                      Column('created_by', Integer),
-                      Column('start_time', DateTime),
-                      Column('end_time', DateTime),
-                      )
 
 group_users = Table('group_users', metadata,
                     Column_id(),
