9fb694e64fa4dd6a4fea97193469e6eca33dc9ec
Shekhar <pythonic@gmail.com>
2015-10-16 18:18:08 +0530

    configurable db max connections

--- appbase/pw.py
+++ appbase/pw.py
@@ -11,7 +11,7 @@ db = PooledPostgresqlExtDatabase(
     host=settings.DB_HOST,
     user=settings.DB_USER,
     password=settings.DB_PASSWORD,
-    max_connections=32,
+    max_connections=settings.DB_MAXCONNECTIONS,
     register_hstore=False,
     stale_timeout=60*2) # 2 minutes
 
@@ -19,6 +19,7 @@ db = PooledPostgresqlExtDatabase(
 class BaseModel(Model):
     class Meta:
         database = db
+        only_save_dirty = True
 
     def to_dict(self, only=None):
         return model_to_dict(self, only=only, recurse=False)
