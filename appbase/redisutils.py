642dfad112eb704dffb035ce3e04a9ccda7bf8a7
Gaurav <gaurav@scroll.in>
2017-08-02 17:20:03 +0530

    StrictRedis => Redis

--- appbase/redisutils.py
+++ appbase/redisutils.py
@@ -1,5 +1,10 @@
+import sys
 import redis
 
 from settings import REDIS_HOST, REDIS_PORT, REDIS_DB
 
-rconn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+if sys.version[0] == '2':
+    rconn = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+else:
+    rconn = redis.Redis(
+        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
