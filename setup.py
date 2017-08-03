a85f5c3c89efc62da9b2593e886c4ed4a00598d6
Shekhar <pythonic@gmail.com>
2017-08-03 17:30:24 +0530

    Choosing 0.0.7 as first serious version of appbase as appbase while stable and in production still isn't 1.0 ready

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='appbase',
-    version='0.1.0',
+    version='0.0.7',
     url="https://bitbucket.com/shon/appbase",
     classifiers=[
         'Programming Language :: Python',
